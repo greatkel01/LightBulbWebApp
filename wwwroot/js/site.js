e26dc2008ea3e4f98d242620a6eb40479ccb53d0
greatkel01 <125046912+greatkel01@users.noreply.github.com>
2023-04-26 04:27:51 +0100

    initial commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function() {
+    const url = "https://lightbulbtiggerfunction.azurewebsites.net/api/lightsensordata";
+
+    // Fetch JSON and apply syntax highlighting
+    fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        const jsonElement = document.getElementById("json");
+        jsonElement.textContent = JSON.stringify(json, null, 2);
+        hljs.highlightBlock(jsonElement);
+      })
+      .catch(error => console.error(error));
+  });
