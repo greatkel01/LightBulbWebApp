5ff19ad0340f34a0d5bade7da1aa9c322ffbfc61
greatkel01 <125046912+greatkel01@users.noreply.github.com>
2023-04-26 04:42:01 +0100

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,16 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-document.addEventListener("DOMContentLoaded", function() {
-    const url = "https://lightbulbtiggerfunction.azurewebsites.net/api/lightsensordata";
+// document.addEventListener("DOMContentLoaded", function() {
+//     const url = "https://lightbulbtiggerfunction.azurewebsites.net/api/lightsensordata";
 
-    // Fetch JSON and apply syntax highlighting
-    fetch(url)
-      .then(response => response.json())
-      .then(json => {
-        const jsonElement = document.getElementById("json");
-        jsonElement.textContent = JSON.stringify(json, null, 2);
-        hljs.highlightBlock(jsonElement);
-      })
-      .catch(error => console.error(error));
-  });
+//     // Fetch JSON and apply syntax highlighting
+//     fetch(url)
+//       .then(response => response.json())
+//       .then(json => {
+//         const jsonElement = document.getElementById("json");
+//         jsonElement.textContent = JSON.stringify(json, null, 2);
+//         hljs.highlightBlock(jsonElement);
+//       })
+//       .catch(error => console.error(error));
+//   });
